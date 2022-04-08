00457b821cec0d41c5ed258716c843b08c6bc635
dax-side <damade615@gmail.com>
2022-04-08 13:06:39 -0700

    new commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,8 +14,7 @@
 */
 int main(void)
 {
-	int n;
-	
+	int n;	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;			
 	if (n > 0)
@@ -24,8 +23,7 @@ int main(void)
 	}				
 	else if (n == 0)
 	{	
-		printf("%d is zero\n", n);						
-	}		
+		printf("%d is zero\n", n);						}		
 	else
 	{
 		printf("%d is negative\n", n);						}
