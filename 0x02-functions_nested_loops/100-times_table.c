2e11109f08b7caa61a5b01f9dc305bf89d01dc7a
dax-side <damade615@gmail.com>
2022-04-12 10:01:07 -0700

    new commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,36 +17,38 @@ void print_times_table(int n)
 	{
 		for (row = 0; row <= n; row++)
 		{
-			product = row * column;
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
 
-			if (column == 0)
-				_putchar('0');
-			else if (product < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product % 10 + '0');
-			}
-			else if (product >= 10 && product < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((product / 10) % 10 + '0');
-				_putchar(product % 10 + '0');
-			}
-			else if (product > 99 && product < 1000)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product / 100 + '0');
-				_putchar((product / 10) % 10 + '0');
-				_putchar(product % 10 + '0');
+				if (column == 0)
+					_putchar('0');
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
 			}
 		}
-		_putchar('\n');
 	}
 }
