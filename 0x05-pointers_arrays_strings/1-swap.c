a4db5022889f742a04e5ffccdc0168aa5371cbb5
dax-side <damade615@gmail.com>
2022-04-20 12:31:40 -0700

    new fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/*
+/**
  * swap_int - swap the values of two integers
  * @a: first value
  * @b: second value
- */
+*/
 
 void swap_int(int *a, int *b)
 {
 	int c;
-	
+
 	c = *a;
 	*a = *b;
 	*b = c;
