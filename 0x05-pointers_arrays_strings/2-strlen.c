f9ad3beb038a9de316fddb4d878d6a1c709043de
dax-side <damade615@gmail.com>
2022-04-20 12:33:45 -0700

    new commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/*
+/**
  * _strlen - returns the length of the string
  * @s: input string to count
  * Description: returns the length of a given string
  * Return: length of string as int
- */
+*/
 
 int _strlen(char *s)
 {
