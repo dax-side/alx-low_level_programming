d0e0034e7e08bf73fabab22e5fb46db684720caf
dax-side <damade615@gmail.com>
2022-04-23 07:52:57 -0700

    new commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,60 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- * cap_string - capitalizes every word of a string
- * @s: string to modify
- *
- * Return: the resulting string
+ * upper - capitalizes a letter if it is lowercase
+ * @c: pointer to a string
+ * Return: pointer to string
 */
-
-char *cap_string(char *s)
+char *upper(char *c)
 {
-	int i, j;
+	if (c[0] >= 'a' && c[0] <= 'z')
+		c[0] -= 32;
+	return (c);
+}
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
+/**
+ * new_word - compares char to chars that indicate a new word
+ * @c: char to check
+ * Return: 1 if char is a word separator, 0 of not
+*/
+int new_word(char c)
+{
+	int i = 0;
+	char *word = " \t\n,;.!?\"(){}";
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (word[i] != '\0')
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-		for (j = 0; j < 13; j++)
+		if (c == word[i])
+			return (1);
+		i++;
+	}
+
+	return (0);
+}
+
+/**
+ * cap_string - capitalize all words in a string
+ * @str: string to modify
+ * Return: pointer to str
+*/
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	upper(str);
+	if (str[0] == '\0')
+		return (str);
+
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (new_word(str[i - 1]) == 1)
 		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-			}
+			upper(&str[i]);
 		}
 	}
+	return (str);
 }
