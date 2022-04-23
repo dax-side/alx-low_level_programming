a283dbf898e586e3e4e198c5aa1451e57bd34ae7
dax-side <damade615@gmail.com>
2022-04-23 06:17:19 -0700

    new commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,18 +14,19 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; j < n; j++)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
+		j++;
 	}
 
-	if (dest[i - 1] != '\0')
-		dest[i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
