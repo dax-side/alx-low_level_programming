990da28c6dce7c44f34d381befe3722feec73ff6
dax-side <damade615@gmail.com>
2022-04-28 12:38:16 -0700

    new~

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,21 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
+ * Description: print string using recursion
+ * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	int i;
+
+	i = 0;
+
+	if (s[i] != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar('\n');
 }
