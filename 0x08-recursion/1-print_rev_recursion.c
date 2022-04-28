599e014a95225d4ae06c80dc4a87ff0ff0fb86b2
dax-side <damade615@gmail.com>
2022-04-28 13:44:55 -0700

    new

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,22 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts_recursion - main - check the code
- * @s: character of string
- * Return: Always 0.
- *
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*(s) != '\0')
+	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	if (*(s) == '\0')
-	{
-		_putchar('\n');
 	}
 }
