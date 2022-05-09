46f19523ca97464141c4b261acdc7b771139d0bf
dax-side <damade615@gmail.com>
2022-05-09 11:57:46 -0700

    new commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,23 +1,19 @@
+#include "main.h"
 #include <stdio.h>
-
-int main(void)
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate a space
+ * @b: unsigned int
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
 {
-	int n;
-	int a[5];
-	int *p;
+void *x;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	p[5] = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+x = malloc(b);
+if (x == NULL)
+{
+	exit(98);
+}
+return (x);
 }
